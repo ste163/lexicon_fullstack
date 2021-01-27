ba9e2542829abc0c4e95eaec69413ab1e7187861
ste163 <samtedwards@pm.me>
2021-01-27 18:42:30 -0500

    completed SQL db generation script

--- Lexicon/SQL/01_Db_Create.sql
+++ Lexicon/SQL/01_Db_Create.sql
@@ -10,8 +10,8 @@ GO
 
 DROP TABLE IF EXISTS [User];
 DROP TABLE IF EXISTS [Project];
-DROP TABLE IF EXISTS [Categorization];
 DROP TABLE IF EXISTS [Collection];
+DROP TABLE IF EXISTS [Categorization];
 DROP TABLE IF EXISTS [ProjectCollection];
 DROP TABLE IF EXISTS [Word];
 GO
@@ -31,13 +31,22 @@ CREATE TABLE [Project] (
 	[CreationDate] datetime NOT NULL,
 	[Name] nvarchar(255) NOT NULL
 
+	CONSTRAINT UQ_Name UNIQUE([Name]),
 	CONSTRAINT [FK_Project_User] FOREIGN KEY ([UserId]) REFERENCES [User] ([Id])
 )
 
 CREATE TABLE [Collection] (
-	
+	[Id] integer PRIMARY KEY IDENTITY,
+	[UserId] integer NOT NULL,
+	[CategorizationId] integer NOT NULL,
+	[CreationDate] datetime NOT NULL,
+	[Name] nvarchar(255) NOT NULL,
+	[Description] nvarchar(255) NOT NULL,
+	[Pinned] bit NOT NULL
 
-
+	CONSTRAINT UQ_Name UNIQUE([Name]),
+	CONSTRAINT [FK_Collection_User] FOREIGN KEY ([UserId]) REFERENCES [User] ([Id]),
+	CONSTRAINT [FK_Collection_Categorization] FOREIGN KEY ([CategorizationId]) REFERENCES [Categorization] ([Id])
 )
 
 CREATE TABLE [Categorization] (
@@ -50,54 +59,19 @@ CREATE TABLE [ProjectCollection] (
 	[ProjectId] integer NOT NULL,
 	[CollectionId] integer NOT NULL
 
-
+	CONSTRAINT [FK_ProjectCollection_Project] FOREIGN KEY ([ProjectId]) REFERENCES [Project] ([Id]),
+	CONSTRAINT [FK_ProjectCollection_Collection] FOREIGN KEY ([CollectionId]) REFERENCES [Collection] ([Id])
 )
 
 CREATE TABLE [Word] (
+	[Id] integer PRIMARY KEY IDENTITY,
+	[UserId] integer NOT NULL,
+	[CollectionId] integer NOT NULL,
+	[MwWordId] integer NOT NULL,
+	[Word] varchar(255) NOT NULL,
+	[LastViewed] datetime NOT NULL
 
+	CONSTRAINT [FK_Word_User] FOREIGN KEY ([UserId]) REFERENCES [User] ([Id]),
+	CONSTRAINT [FK_Word_Collection] FOREIGN KEY ([CollectionId]) REFERENCES [Collection] ([Id])
 )
 GO
-
-	
-
--- BELOW IS FROM TABLOID
-CREATE TABLE [Post] (
-  [Id] integer PRIMARY KEY IDENTITY,
-  [Title] nvarchar(255) NOT NULL,
-  [Content] text NOT NULL,
-  [ImageLocation] nvarchar(255),
-  [CreateDateTime] datetime NOT NULL,
-  [PublishDateTime] datetime,
-  [IsApproved] bit NOT NULL,
-  [CategoryId] integer NOT NULL,
-  [UserProfileId] integer NOT NULL,
-
-  CONSTRAINT [FK_Post_Category] FOREIGN KEY ([CategoryId]) REFERENCES [Category] ([Id]),
-  CONSTRAINT [FK_Post_UserProfile] FOREIGN KEY ([UserProfileId]) REFERENCES [UserProfile] ([Id])
-)
-
-CREATE TABLE [Comment] (
-  [Id] integer PRIMARY KEY IDENTITY,
-  [PostId] integer NOT NULL,
-  [UserProfileId] integer NOT NULL,
-  [Subject] nvarchar(255) NOT NULL,
-  [Content] text NOT NULL,
-  [CreateDateTime] datetime NOT NULL,
-
-  CONSTRAINT [FK_Comment_Post] FOREIGN KEY ([PostId]) REFERENCES [Post] ([Id]),
-  CONSTRAINT [FK_Comment_UserProfile] FOREIGN KEY ([UserProfileId]) REFERENCES [UserProfile] ([Id])
-)
-
-CREATE TABLE [Tag] (
-  [Id] integer PRIMARY KEY IDENTITY,
-  [Name] nvarchar(50) NOT NULL
-)
-
-CREATE TABLE [PostTag] (
-  [id] integer PRIMARY KEY IDENTITY,
-  [PostId] integer NOT NULL,
-  [TagId] integer NOT NULL,
-  
-  CONSTRAINT [FK_PostTag_Post] FOREIGN KEY ([PostId]) REFERENCES [Post] ([Id]),
-  CONSTRAINT [FK_PostTag_Tag] FOREIGN KEY ([TagId]) REFERENCES [Tag] ([Id])
-)
