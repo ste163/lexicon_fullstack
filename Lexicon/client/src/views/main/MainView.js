7b1c83db69c68449f9535c83cbaa53eb6981d5fd
ste163 <samtedwards@pm.me>
2021-01-29 18:27:53 -0500

    tracking window dimension state

--- Lexicon/client/src/views/main/MainView.js
+++ Lexicon/client/src/views/main/MainView.js
@@ -5,23 +5,43 @@ import './MainView.css'
 
 const MainView = () => {
     // Track browser windows dimensions, so if they are below a certain amount, swap to mobile-view header
-    const [ windowDimensions, setWindowDimensions ] = useState({ height: window.innerHeight, width: window.innerWidth})
+    const [ windowDimensions, setWindowDimensions ] = useState({ height: window.innerHeight, width: window.innerWidth })
     
     // Need to track the state of List, Selected, and Thesaurus Columns
     // Based on if they are "True" display their columns. If not, display: none
     // Will also need to turn them on and off based on Screen Width. Do not handle that with CSS because they could conflict
 
+    // Debounce and useEffect based on https://www.pluralsight.com/guides/re-render-react-component-on-window-resize
+    // Debounce delays re-renders on every resize event
+    function debounce(fn, ms) {
+        let timer
+        return _ => {
+          clearTimeout(timer)
+          timer = setTimeout(_ => {
+            timer = null
+            fn.apply(this, arguments)
+          }, ms)
+        };
+      }
 
     useEffect(() => {
-        handleResize()
-    }, [])
+        const debouncedHandleResize = debounce(function handleResize() {
+            setWindowDimensions({
+              height: window.innerHeight,
+              width: window.innerWidth
+            })
+          }, 300)
 
-    const handleResize = () => {
-        console.log('resized to: ', window.innerWidth, 'x', window.innerHeight)
-    }
+        // EventListener on Window object to properly track the current browser dimensions
+        window.addEventListener('resize', debouncedHandleResize)
+        console.log(windowDimensions)
 
-    // EventListener on Window object to properly track the current browser dimensions
-    window.addEventListener('resize', handleResize)
+        // Remove event listener so we don't add an infinite amount
+        return _ => {
+            window.removeEventListener('resize', debouncedHandleResize)
+        }
+    }, [windowDimensions])
+
 
     return (
         <div className="app__container">
