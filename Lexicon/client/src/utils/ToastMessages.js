b4870f10a8bbe759ebd273a09fee3f92055138b5
ste163 <samtedwards@pm.me>
2021-01-31 21:34:19 -0500

    refactored toast messages into one-liners

--- Lexicon/client/src/utils/ToastMessages.js
+++ Lexicon/client/src/utils/ToastMessages.js
@@ -1,34 +1,17 @@
 import React from 'react'
 
-export const Warning = () => {
-    return 'DEFAULT WARNING MSG'
-}
+export const Warning = () => 'DEFAULT WARNING MSG'
 
-export const RegSuccess = () => {
-    return "Registered! Welcome to Lexicon."
-}
+export const RegSuccess = () => "Registered! Welcome to Lexicon."
 
-export const RegFail = () => {
-    return "This email has already been registered."
-}
+export const RegFail = () => "This email has already been registered."
 
-export const LoginWelcome = () => {
-    return "Welcome to Lexicon!"
-}
+export const LoginWelcome = () => "Welcome to Lexicon!"
 
-export const LoginError = () => {
-    return "Invalid email or password."
-}
+export const LoginError = () =>  "Invalid email or password."
 
-export const AnonWelcome = () => {
-    return "Welcome! As an anonymous user, you can not save data."
-}
+export const AnonWelcome = () =>  "Welcome! As an anonymous user, you can not save data."
 
-export const AnonError = () => {
-    return "Error: unable to sign in anonymously."
-}
+export const AnonError = () => "Error: unable to sign in anonymously."
 
-
-export const DbNoConnection = () => {
-    return "Could not connect to Lexicon's database."
-}
+export const DbNoConnection = () => "Could not connect to Lexicon's database."
