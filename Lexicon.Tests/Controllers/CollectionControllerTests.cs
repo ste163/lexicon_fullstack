0a55adb2973b8a44684f2f4b5ced58aeca96ac10
ste163 <samtedwards@pm.me>
2021-01-31 19:09:45 -0500

    registered user can get their posts is passing

--- Lexicon.Tests/Controllers/CollectionControllerTests.cs
+++ Lexicon.Tests/Controllers/CollectionControllerTests.cs
@@ -28,13 +28,30 @@ namespace Lexicon.Tests.Controllers
             // You do not have to have the full object coming back with all the required NOT NULL, etc.
             // We are not faking a db, just the items that would come out of it.
             _fakeCollectionRepo = new Mock<ICollectionRepository>();
-            _fakeCollectionRepo.Setup(r => r.Get(It.Is<int>(i => i == 1))).Returns((int id) => new List<Collection>() { new Collection() { Id = id, Name = "Scary places", Description = "Spooky places It probably hangs at.",} });
+            // Whenever a user with this Id calls the Get method, return this list of collections
+            _fakeCollectionRepo.Setup(r => r.Get(It.Is<int>(i => i == 1))).Returns((int id) => new List<Collection>() { new Collection() { Id = 1, Name = "Scary places", Description = "Spooky places It probably hangs at."}, new Collection() { Id = 2, Name = "Monsters", Description = "Monsters It becomes." } });
+            _fakeCollectionRepo.Setup(r => r.Get(It.Is<int>(i => i == 2))).Returns((int id) => new List<Collection>() { new Collection() { Id = 1, Name = "Swampy", Description = "Icky words related to swamps" }, new Collection() { Id = 2, Name = "Spooky", Description = "Spooky related words." } });
         }
 
         [Fact]
         public void Registered_User_Can_Get_Posts()
         {
 
+            // Spoof an authenticated user by generating a ClaimsPrincipal
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[] {
+                                   new Claim(ClaimTypes.NameIdentifier, "FIREBASE_USER1"),
+                                   }, "TestAuthentication"));
+
+            // Spoof UserController
+            var controller = new CollectionController(_fakeUserRepo.Object, _fakeCollectionRepo.Object);
+            controller.ControllerContext = new ControllerContext(); // Required to create the controller
+            controller.ControllerContext.HttpContext = new DefaultHttpContext { User = user }; // Pretend the user is making a request to the controller
+
+            // Attempt to Get this User's posts
+            var response = controller.GetByUserId();
+
+            // Returns Ok
+            Assert.IsType<OkObjectResult>(response);
         }
 
         [Fact]
