fc630d9859bc17ab148a7e623f35b7570c45f26c
ste163 <samtedwards@pm.me>
2021-02-03 10:03:30 -0500

    abstracted routes

--- Lexicon/client/src/utils/Routes.js
+++ Lexicon/client/src/utils/Routes.js
@@ -3,19 +3,19 @@ export const AuthRoute = () => "/auth"
 
 export const AppRoute = () => "/app"
 
-export const AppSelectedRoute = pathnameId => `/app/selected/${pathnameId}`
+export const AppSelectedRoute = pathnameId => `${AppRoute()}/selected/${pathnameId}`
 
-export const SettingsRoute = () => "/app/settings"
+export const SettingsRoute = () => `${AppRoute()}/settings`
 
-export const CollectionManagerRoute = () => "/app/collection-manager"
+export const CollectionManagerRoute = () => `${AppRoute()}/collection-manager`
 
-export const CollectionManagerCreateRoute = () => "/app/collection-manager/create"
+export const CollectionManagerCreateRoute = () => `${CollectionManagerRoute()}/create`
 
-export const CollectionManagerDetailsRoute = pathnameId => `/app/collection-manager/details/${pathnameId}`
+export const CollectionManagerDetailsRoute = pathnameId => `${CollectionManagerRoute()}/details/${pathnameId}`
 
-export const CollectionManagerEditRoute = pathnameId => `/app/collection-manager/edit/${pathnameId}`
+export const CollectionManagerEditRoute = pathnameId => `${CollectionManagerRoute()}/edit/${pathnameId}`
 
-export const CollectionManagerDeleteRoute = pathnameId => `/app/collection-manager/delete/${pathnameId}`
+export const CollectionManagerDeleteRoute = pathnameId => `${CollectionManagerRoute()}/delete/${pathnameId}`
 
 export const findRouteParam = (currentUrl) => {
     const regex = /\d+/
