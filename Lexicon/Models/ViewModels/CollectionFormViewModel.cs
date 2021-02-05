ec6515e05b9c523d4d9bea3ef38078553932687f
ste163 <samtedwards@pm.me>
2021-02-04 20:19:11 -0500

    added view model for the Collection Form

---
+++ Lexicon/Models/ViewModels/CollectionFormViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Lexicon.Models.ViewModels
+{
+    public class CollectionFormViewModel
+    {
+        public Collection Collection { get; set; }
+        public List<ProjectCollection> ProjectCollections { get; set; }
+    }
+}
