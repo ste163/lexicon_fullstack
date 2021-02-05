f5411c8b168cc5413ac10ee5175cb72bbf1ce7f4
ste163 <samtedwards@pm.me>
2021-02-04 20:34:31 -0500

    added repo to startup; added repo methods, needs testing

--- Lexicon/Repositories/IProjectCollectionRepository.cs
+++ Lexicon/Repositories/IProjectCollectionRepository.cs
@@ -1,24 +1,18 @@
-﻿using System;
+﻿using Lexicon.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Lexicon.Repositories
 {
-    public class IProjectCollectionRepository
+    public interface IProjectCollectionRepository
     {
-        // Get ProjectCollections for the current Project (by id)
-        // Get ProjectCollections for the current Collection (by id)
-        // Add
-        // Delete
+        List<ProjectCollection> GetByCollectionId(int id);
+        List<ProjectCollection> GetByProjectId(int id);
+        void Add(List<ProjectCollection> projectCollections);
+        void Delete(List<ProjectCollection> projectCollections);
 
-        // In our GetByIds in the Project and Collection controllers,
-        // always run the get ProjCol by Id from here!
-
-        // Ensure Project and Collection forms have a GetByUserId for the other's full list of info
-        // to populate the drop-down. -- Use a drop-down menu for now, but I'd like to have a cleaner list
-        // of them
-        // MIGHT look good if they're grey buttons with X's to the right of them. That list out just like word buttons do.
-        // I could then repeat these "Delete Buttons" when I hit "Edit" on a Collection so you could quickly delet un-wanted words
+        // Project and Collection controllers, always run the get ProjCol by Id
     }
 }
