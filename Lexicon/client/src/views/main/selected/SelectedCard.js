0cacd9defbc328de79082b62bc0705cda4c7cf57
ste163 <samtedwards@pm.me>
2021-02-09 18:26:24 -0500

    displaying words alphabetically by default

--- Lexicon/client/src/views/main/selected/SelectedCard.js
+++ Lexicon/client/src/views/main/selected/SelectedCard.js
@@ -1,43 +1,65 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { WordButton, DeleteWordButton } from '../../../components/buttons/Buttons'
+import { SearchBar } from '../../../components/inputs/Inputs'
 import { WordDeleteRoute } from "../../../utils/Routes"
 import "./SelectedCard.css"
 
 const SelectedCard = ({ selectedCollection, getWordFromThesaurus, history }) => {
     const [ isDeletingWords, setIsDeletingWords ] = useState(false)
-console.log(selectedCollection)
+
+    const [filteredWords, setFilteredWords] = useState([])
+
+    useEffect(() => {
+        setFilteredWords([...selectedCollection.words])
+    }, [selectedCollection])
+    
+    const sortedAlphabetically = (words) => {
+        const sorted = words.sort((a,b) => {
+            const wordA = a.name.toLowerCase()
+            const wordB = b.name.toLowerCase()
+            if (wordA < wordB) {
+                return -1
+            }
+            if (wordB > wordA) {
+                return 1
+            }
+            return 0
+        })
+        return sorted
+    }
+
     return (
         <article className="card card__color--white card__selected">
             {/* <SelectedDotMenu ref={dotMenu} collection={selectedCollection} /> */}
     
-            <div className="card__type">
-                Selected Collection
-            </div>
-            <h1 className="selected__h1">
-                {selectedCollection.collection.name}
-            </h1>
-            <p className="selected__description">
-                {selectedCollection.collection.description}
-            </p>
+            <div className="card__type">Selected Collection</div>
+            <h1 className="selected__h1">{selectedCollection.collection.name}</h1>
+            <p className="selected__description">{selectedCollection.collection.description}</p>
     
-            {/* 
-                IF NO WORDS IN COLLECTION YET,
-                INFORM USER TO SEARCH THESAURUS TO ADD WORDS
-            */}
-    
-            {/* TO BE REPlACED BY THE SEARCH BAR COMPONENT */}
-            <fieldset className="selected__search">
-                <label htmlFor="collectionSearch">Search words in collection:</label>
-                <input type="text"
-                name="collectionSearch"
-                className="input__search"
-                placeholder="Search for word or part of speech... " />
-            </fieldset>
+            {/* <div className="selected__search">
+                <SearchBar
+                labelTitle={"Search for words in collection:"}
+                placeholderText={"Search for word..."}
+                setSearchTerms={"test"} />
+            </div> */}
     
             {/* DROPDOWN FOR CATEGORIZATION TYPE */}
             <fieldset className="selected__categorization">
                 <label htmlFor="collectionSelect">Categorize by:</label>
-                <select name="collectionSelect" id="collectionSelect">
+                <select
+                    name="collectionSelect"
+                    id="collectionSelect"
+                    defaultValue={"1"}
+                    onChange={e => {
+                        if (e.target.value === "1") {
+                            const words = [...filteredWords]
+                            const sorted = sortedAlphabetically(words)
+                            setFilteredWords(sorted)
+                        }
+                        if (e.target.value === "2") {
+                            console.log("SORT PART")
+                        }
+                    }} >
                     <option value="1">Alphabetically</option>
                     <option value="2">Part of Speech</option>
                 </select>
@@ -62,13 +84,13 @@ console.log(selectedCollection)
             
                     <section className="selected__words word__list definition__words">
                         {!isDeletingWords ? (
-                            selectedCollection.words.map(w => <WordButton
+                            filteredWords.map(w => <WordButton
                                                             key={w.id}
                                                             word={w.name}
                                                             getWordFromThesaurus={getWordFromThesaurus}
                                                             isSelectedCard={true} />)
                         ) : (
-                            selectedCollection.words.map(w => <DeleteWordButton
+                            filteredWords.map(w => <DeleteWordButton
                                                             key={w.id}
                                                             wordId={w.id}
                                                             word={w.name}
