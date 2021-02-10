6c99c13a2a8dea3b40dfc0d52dcd9d59b1871ee8
ste163 <samtedwards@pm.me>
2021-02-10 15:58:24 -0500

    anonymous user gets different menu options

--- Lexicon/client/src/components/headerDesktop/subHeader/SubHeader.js
+++ Lexicon/client/src/components/headerDesktop/subHeader/SubHeader.js
@@ -12,53 +12,62 @@ const SubHeader = ({
     selectedCollection,
     appSelectedRoute,
     history,
-    collections }) => (
-    <section className="view__subHeader">
-        <ManagerButton
-            isMobile={false}
-            managerRoute={CollectionManagerRoute}
-            managerBtnText={"Manage Collections"} />
+    collections }) => {
+    const currentUserId = +sessionStorage.getItem('currentUserId') // If 0, then anonymous, do not allow user to do anything
 
-        <ManagerButton
-            isMobile={false}
-            managerRoute={ProjectManagerRoute}
-            managerBtnText={"Manage Projects"} />
+    return (
+        <section className="view__subHeader">
+            <ManagerButton
+                isMobile={false}
+                managerRoute={CollectionManagerRoute}
+                managerBtnText={"Manage Collections"} />
 
-        <div className="line__vertical"></div>
+            <ManagerButton
+                isMobile={false}
+                managerRoute={ProjectManagerRoute}
+                managerBtnText={"Manage Projects"} />
 
-        <DropDown
-            nameOf='Selected collection'
-            fieldsetLocation='subHeader__fieldset'
-            labelIdName='collectionSelect'
-            isCollection={true}
-            history={history}
-            urlToPushTo={appSelectedRoute}
-            currentState={selectedCollection}
-            stateArray={collections} />
-
-        <div className="line__vertical"></div>
-
-        <label className="toggle__column" htmlFor="listColumn">Toggle List Column: </label>
-
-        <input
-            disabled={!collections || collections.length === 0 ? true : false} // Disable toggle if no collections
-            onChange={e => {setIsListColumnActive(e.currentTarget.checked)}}
-            defaultChecked={!collections || collections.length === 0 ? true : isListColumnActive} // need default checked to stop react error
-            type="checkbox"
-            id="listColumn"
-            name="listColumn"
-            value="listColumn" />
-
-        <label className="toggle__column" htmlFor="listColumn">Toggle Selected Column: </label>
-        <input
-            onChange={e => {setIsSelectedColumnActive(e.currentTarget.checked)}}
-            checked={isSelectedColumnActive}
-            type="checkbox"
-            id="listColumn"
-            name="listColumn"
-            value="listColumn" />
-
-    </section>
-)
+            {currentUserId === 0 ? (
+                null
+            ) : (
+                <>
+                    <div className="line__vertical"></div>
+        
+                    <DropDown
+                        nameOf='Selected collection'
+                        fieldsetLocation='subHeader__fieldset'
+                        labelIdName='collectionSelect'
+                        isCollection={true}
+                        history={history}
+                        urlToPushTo={appSelectedRoute}
+                        currentState={selectedCollection}
+                        stateArray={collections} />
+        
+                    <div className="line__vertical"></div>
+        
+                    <label className="toggle__column" htmlFor="listColumn">Toggle List Column: </label>
+        
+                    <input
+                        disabled={!collections || collections.length === 0 ? true : false} // Disable toggle if no collections
+                        onChange={e => {setIsListColumnActive(e.currentTarget.checked)}}
+                        defaultChecked={!collections || collections.length === 0 ? true : isListColumnActive} // need default checked to stop react error
+                        type="checkbox"
+                        id="listColumn"
+                        name="listColumn"
+                        value="listColumn" />
+        
+                    <label className="toggle__column" htmlFor="listColumn">Toggle Selected Column: </label>
+                    <input
+                        onChange={e => {setIsSelectedColumnActive(e.currentTarget.checked)}}
+                        checked={isSelectedColumnActive}
+                        type="checkbox"
+                        id="listColumn"
+                        name="listColumn"
+                        value="listColumn" />
+                </>
+            )}
+        </section>
+    )
+}
 
 export default SubHeader
