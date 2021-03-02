0c5389b7697e477385677c9ee844e3b656552a69
ste163 <samtedwards@pm.me>
2021-03-02 12:58:56 -0500

    removed backend & firebase

--- Lexicon/SQL/02_Seed_Data.sql
+++
@@ -1,8 +0,0 @@
--- Fill this out after we have some data and can make sure everything works
-set identity_insert [Categorization] on
-insert into [Categorization] ([Id], [Type]) VALUES (1, 'Alphabetical'), (2, 'Part of Speech')
-set identity_insert [Categorization] off
-
-set identity_insert [User] on
-insert into [User] ([Id], [FirebaseUserId], [Email]) VALUES (1, 'ChangeToRealOne', 'admin@email.com')
-set identity_insert [User] off
--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import firebase from 'firebase/app'
 import Lexicon from './Lexicon'
 // Index.js is Lexicon's entry point
-
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-}
-firebase.initializeApp(firebaseConfig)
 
 ReactDOM.render(
   <React.StrictMode>
